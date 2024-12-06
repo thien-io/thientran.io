30b0fbef1e8bed3f77913fe2a9c4b64bf9bc4361
thienjs <thien.tran6@gmail.com>
2024-12-06 01:43:53 -0500

    footer bottom

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -40,7 +40,7 @@ function SocialLinks() {
 
 export default function Footer() {
   return (
-    <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
+    <small className="  text-[#1C1C1C] dark:text-[#D4D4D4] mt-auto mb-5">
       <time>© {YEAR}</time>{" "}
       <a
         className="no-underline"

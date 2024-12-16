f8eff26415ac0113c63bda82dbc58b8ba84da714
Thien Tran <thien.tran6@gmail.com>
2024-12-16 15:42:09 +0000

    hero tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { socialLinks } from "./config";
 import { NameTransition } from './components/name';
+import { HeroTabs } from "./components/hero-tabs";
 
 export default function Page() {
   return (
     <section>
       <div className='prose prose-neutral dark:prose-invert'>
         <p>Hello, I'm Thien, a tennis coach from Connecticut.</p>
+        <HeroTabs />
       </div>
     </section>
   );

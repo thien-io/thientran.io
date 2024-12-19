feef81f6a37b5bb1c5d9fa358fdfcec6cf6c86fd
thienjs <thien.tran6@gmail.com>
2024-12-19 00:42:52 -0500

    lag

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -2,16 +2,13 @@ import BlurFade from '@/components/ui/blur-fade';
 
 export default function About() {
   return (
-    <section>
-
-      <BlurFade delay={0.2}>
-        <p className='text-sm mb-4'>
-          I'm a USPTA-certified tennis coach and passionate web
-          developer/designer. With over 20 years of experience on the court, I
-          specialize in helping players of all levels improve their skills,
-          build confidence, and enjoy the game. 
-        </p>
-      </BlurFade>
-    </section>
+    <BlurFade delay={0.2}>
+      <p className='text-sm mb-4'>
+        I'm a USPTA-certified tennis coach and passionate web
+        developer/designer. With over 20 years of experience on the court, I
+        specialize in helping players of all levels improve their skills, build
+        confidence, and enjoy the game.
+      </p>
+    </BlurFade>
   );
 }
